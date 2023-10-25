be1b576e5e61383ce1f5c37620e27a1d856d1b78
SardorUrokov <urokovsardor70@gmail.com>
2023-10-25 18:09:05 +0500

    Changes

--- src/main/java/com/example/elastic_search_project/service/CustomerService.java
+++ src/main/java/com/example/elastic_search_project/service/CustomerService.java
@@ -1,39 +1,39 @@
 package com.example.elastic_search_project.service;
 
-import lombok.extern.slf4j.Slf4j;
+import com.example.elastic_search_project.ElasticSearchProjectApplication;
 import lombok.RequiredArgsConstructor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.stereotype.Service;
 import com.example.elastic_search_project.document.Customer;
 import com.example.elastic_search_project.repository.CustomerRepository;
 import com.example.elastic_search_project.exceptions.ResourceNotFoundException;
 
-@Slf4j
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+    private static final Logger log = LogManager.getLogger(ElasticSearchProjectApplication.class);
 
     public Customer save(Customer customer) {
         final var saved = customerRepository.save(customer);
-        logger.info("Customer is saved {}", saved);
+        log.info("Customer is saved {}", saved);
         return saved;
     }
 
     public Iterable<Customer> findAll() {
         final var customers = customerRepository.findAll();
-        logger.info("Getting all Customers data");
+        log.info("Getting all Customers data");
         return customers;
     }
 
     public Customer findById(int id) {
         final var customer = customerRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Customer", "customer_id", id));
-        logger.info("Getting customer by {} id -> {}", id, customer);
+        log.info("Getting customer by {} id -> {}", id, customer);
         return customer;
     }
 
@@ -42,7 +42,7 @@ public class CustomerService {
         customer.setFullName(customer1.getFullName());
         customer.setPhoneNumber(customer1.getPhoneNumber());
         final var saved = customerRepository.save(customer);
-        logger.warn("Customer is updated by {} id -> {}", id, saved);
+        log.warn("Customer is updated by {} id -> {}", id, saved);
         return saved;
     }
 
